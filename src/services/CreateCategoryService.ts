ee7d0ab6f3a64893b57ef3381a0fd4dc8e0434d3
gasorey <gasorey@gmail.com>
2020-04-19 22:01:49 -0300

    challenge

---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,20 @@
+import { getRepository } from 'typeorm';
+import Category from '../models/Category';
+
+interface Request {
+  category: string;
+}
+
+export default class CreateCategoryService {
+  public async execute({ category }: Request): Promise<Category> {
+    const categoryRepository = getRepository(Category);
+    const getCategory = await categoryRepository.findOne(category);
+
+    if (!getCategory) {
+      const newCategory = categoryRepository.create({ title: category });
+      await categoryRepository.save(newCategory);
+      return newCategory;
+    }
+    return getCategory;
+  }
+}
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,7 +1,6 @@
-import { getRepository, getCustomRepository } from 'typeorm';
-import Transaction from '../models/Transaction';
-import Category from '../models/Category';
-import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import CreateCategoryService from './CreateCategoryService';
+
 import TransactionRepository from '../repositories/TransactionsRepository';
 
 interface Request {
@@ -20,24 +19,18 @@ class CreateTransactionService {
   }: Request): Promise<Transaction> {
     const transactionRepository = getCustomRepository(TransactionRepository);
 
-    const categoryRepostory = getRepository(Category);
+    const createCategory = new CreateCategoryService();
 
-    const category_id = await categoryRepostory.findOne(category);
-
-    if (!category_id) {
-      const newCategory_id = await categoryRepostory.create({
-        title: category,
-      });
-      await categoryRepostory.save(newCategory_id);
-    }
+    const getCategory = await createCategory.execute({ category });
 
     const transaction = transactionRepository.create({
       title,
       value,
       type,
-      category: Category,
+      category: getCategory.id,
     });
     await transactionRepository.save(transaction);
+    return transaction;
   }
 }
 
